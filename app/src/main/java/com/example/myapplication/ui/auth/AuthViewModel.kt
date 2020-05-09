c491bde99cc08aef2e08190991c877535da6b3ae
gabriel <gabrielbrogni@hotmail.com>
2020-05-08 22:44:30 -0300

    Check if user is authenticated

--- app/src/main/java/com/example/myapplication/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/auth/AuthViewModel.kt
@@ -1,18 +1,30 @@
 package com.example.myapplication.ui.auth
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import com.example.myapplication.base.BaseViewModel
 import com.example.myapplication.data.model.Contact
+import com.example.myapplication.data.model.Group
 import com.example.myapplication.data.repositories.ContactRepository
+import com.example.myapplication.utils.Coroutines
 
 class AuthViewModel(
     private val contactRepository: ContactRepository
 ) : BaseViewModel() {
+
+    private val _userCreatedAndAuthenticated = MutableLiveData<Boolean>()
+    val userCreatedAndAuthenticated: LiveData<Boolean>
+        get() = _userCreatedAndAuthenticated
+
     enum class AuthenticationState {
         UNAUTHENTICATED,
         AUTHENTICATED,
         INVALID_AUTHENTICATION
+    }
+
+    init {
+        verifyUserCreatedAndAuthenticated()
     }
 
     var authenticationState =
@@ -22,4 +34,14 @@ class AuthViewModel(
     fun getContact() = contactRepository.getContact()
     fun deleteContact() = contactRepository.deleteContact()
     fun getFirebaseAuth() = contactRepository.getFirebaseAuth()
+
+    private fun verifyUserCreatedAndAuthenticated() {
+        Coroutines.ioThenMain(
+            { contactRepository.getContact() },
+            {
+                _userCreatedAndAuthenticated.value =
+                    !it?.id.isNullOrEmpty() && authenticationState == AuthenticationState.AUTHENTICATED
+            }
+        )
+    }
 }
--- app/src/main/java/com/example/myapplication/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/splash/SplashFragment.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
+import com.example.myapplication.data.model.Contact
 import com.example.myapplication.ui.auth.AuthViewModel
+import com.example.myapplication.utils.Coroutines
 import com.example.myapplication.utils.extension.kodeinViewModel
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.kodein
@@ -27,10 +30,12 @@ class SplashFragment : Fragment(), KodeinAware {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        if (viewModel.authenticationState == AuthViewModel.AuthenticationState.AUTHENTICATED) {
-            findNavController().navigate(R.id.action_splashFragment_to_groupsFragment)
-        } else {
-            findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
-        }
+        viewModel.userCreatedAndAuthenticated.observe(viewLifecycleOwner, Observer {
+            if (it) {
+                findNavController().navigate(R.id.action_splashFragment_to_groupsFragment)
+            } else {
+                findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+            }
+        })
     }
 }
